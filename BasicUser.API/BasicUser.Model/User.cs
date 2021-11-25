1c09ac43bdc55770c7dd2de2e01706c29aa6b71f
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-11-26 02:26:36 +0300

    model layer created

---
+++ BasicUser.API/BasicUser.Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicUser.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Kullanici adinizi giriniz.")]
+        [StringLength(maximumLength:50, MinimumLength =3,ErrorMessage ="Kullanici adiniz min 3 ve max 50 karakterden olusabilir.")]
+        public string UserName { get; set; }
+
+        [MaxLength(100, ErrorMessage ="Adiniz max 100 karakter olabilir.")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage ="Gecerli bir mail adresi giriniz.")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage ="Sifreleriniz Uyusmuyor")]
+        public string RePassword { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+    }
+}
