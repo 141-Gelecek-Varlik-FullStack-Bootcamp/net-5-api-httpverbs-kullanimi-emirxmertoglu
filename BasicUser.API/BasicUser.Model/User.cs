cbadb2eaa112aba451405ceb50ec3af1b6c407e1
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-11-26 04:09:57 +0300

    fixed error messages

--- BasicUser.API/BasicUser.Model/User.cs
+++ BasicUser.API/BasicUser.Model/User.cs
@@ -8,22 +8,22 @@ namespace BasicUser.Model
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Kullanici adinizi giriniz.")]
-        [StringLength(maximumLength:50, MinimumLength =3,ErrorMessage ="Kullanici adiniz min 3 ve max 50 karakterden olusabilir.")]
+        [Required(ErrorMessage = "Kullanici adinizi giriniz!")]
+        [StringLength(maximumLength:50, MinimumLength =3,ErrorMessage ="Kullanici adiniz min 3 ve max 50 karakterden olusabilir!")]
         public string UserName { get; set; }
 
-        [MaxLength(100, ErrorMessage ="Adiniz max 100 karakter olabilir.")]
+        [MaxLength(100, ErrorMessage ="Adiniz max 100 karakter olabilir!")]
         public string FullName { get; set; }
 
-        [Required]
-        [EmailAddress(ErrorMessage ="Gecerli bir mail adresi giriniz.")]
+        [Required(ErrorMessage = "Email adresinizi giriniz!")]
+        [EmailAddress(ErrorMessage ="Gecerli bir mail adresi giriniz!")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Sifrenizi giriniz!")]
         public string Password { get; set; }
 
-        [Required]
-        [Compare("Password", ErrorMessage ="Sifreleriniz Uyusmuyor")]
+        [Required(ErrorMessage = "Tekrar sifrenizi giriniz!")]
+        [Compare("Password", ErrorMessage ="Sifreleriniz Uyusmuyor!")]
         public string RePassword { get; set; }
 
         public DateTime RegisterDate { get; set; }
